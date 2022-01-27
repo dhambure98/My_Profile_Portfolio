f12efc982201a6725489d7d826314ca5ae5b7c24
dhambure98 <akila789liyanage@gmail.com>
2022-01-27 23:10:29 +0530

    'Update_some_script_lines'

--- MyProjects/JS/SPA/assets/js/script.js
+++ MyProjects/JS/SPA/assets/js/script.js
@@ -3,6 +3,16 @@
 // * @since : 0.1.0
 
 <!--JavaScript DOM Lines -->
+document.getElementById("customer").addEventListener("click",function () {
+    document.getElementById("txtCustomerId").style.display="none";
+    document.getElementById("txtCustomerName").style.display="none";
+    document.getElementById("txtCustomerName").style.display="none";
+    document.getElementById("txtCustomerSalary").style.display="none";
+    // document.getElementById("orderSec").style.display="none";
+    // document.getElementById("orderName").style.display="none";
+    // document.getElementById("customerSec").style.display="block";
+    // document.getElementById("customerName").style.display="block";
+});
 
 // sidebar-wrapper ---------->
 var el = document.getElementById("wrapper");
